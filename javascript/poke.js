9f49d96827ba2454ee22f755fa3da7f4e6071fff
yumeoi_bit <70701636+fumifumi238@users.noreply.github.com>
2021-11-04 23:23:40 +0900

    Update poke.js

--- javascript/poke.js
+++ javascript/poke.js
@@ -1,5 +1,5 @@
-import pokeName from './poke_name.js'
-console.log(pokeName)
+//import pokeName from './poke_name.js'
+//console.log(pokeName)
 for(let i=0;i<4;i++){
   option = document.createElement('option');
   option.value = `${i}`;
